83dc8682a8c7092a8a8dbca930b21e430cbd50b6
DangoDaniel <107574231+DangoDaniel@users.noreply.github.com>
2023-04-14 20:09:59 +0200

    Delete target directory

--- target/Workshop5-1.0-SNAPSHOT/WEB-INF/classes/META-INF/data.sql
+++
@@ -1,3 +0,0 @@
-insert into workshopHibernate.books(isbn, title, author, publisher, type) values
-('1234', 'Fajna książka o programowaniu', 'Adam Mickiewicz', 'Helion', 'Programowanie'),
-('123456789', 'Harry Potter i Kamień Filozoficzny', 'J.K. Rowling', 'Helion', 'Fantastyka');
